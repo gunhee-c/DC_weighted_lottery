cf2a1d51dcfa2818f9766f76f71bec25cc526582
dnwns3437 <chorjgml@naver.com>
2024-04-04 01:27:08 +0900

    .
    .

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from Weighted_Lottery_Base import *
+
+
+# Title
+st.title('가중치/단계적 추첨')
+st.write('이 페이지는 가중치를 두고  ticket and the item with the winning ticket will be displayed.')
